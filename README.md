ddfb5c00c180b9b49630150d566f2fd47c06275f
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2024-04-22 12:19:33 -0700

    :tada: mvp

---
+++ README.md
@@ -0,0 +1,24 @@
+# Blender Gamepad Input Addon
+
+An example of how to get gamepad and other device input (like MIDI maybe) and use it inside Blender.
+
+> On some devices like Mac you may need to give elevated permissions to Blender to allow for plugin to work.
+
+## How to use
+
+1. Download as a zip
+1. Open Blender
+1. Go to Edit > Preferences and go to the Addons tab on left.
+1. Click install button.
+1. Select the zip you downloaded.
+1. You can confirm it's installed by searching for "Gamepad" and seeing if it's checked off
+
+## How it works
+
+TBD.
+
+Uses `inputs` Python library to get gamepad input data across all platforms (Windows, Linux, and Mac).
+
+## Credits
+
+- [inputs](https://github.com/zeth/inputs) by @zeth
---
+++ __init__.py
@@ -0,0 +1,104 @@
+bl_info = {
+    "name": "Gamepad Input Example",
+    "description": "",
+    "author": "whoisryosuke",
+    "version": (0, 0, 2),
+    "blender": (2, 80, 0),
+    "location": "Properties > Output",
+    "warning": "", # used for warning icon and text in addons panel
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Development"
+}
+
+# __is_first_load = "operators" not in locals()
+# if __is_first_load:
+#     from .inputs import devices
+# else:
+#     import importlib
+
+#     inputs = importlib.reload(inputs)
+
+# from . import inputs
+from .inputs import devices
+
+import bpy
+
+class GamepadInputPanel(bpy.types.Panel):
+    """Creates a Panel in the scene context of the properties editor"""
+    bl_label = "Gamepad Input Example"
+    bl_idname = "SCENE_PT_gamepad"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_context = "output"
+
+    def draw(self, context):
+        layout = self.layout
+
+        scene = context.scene
+
+        # Create a simple row.
+        layout.label(text=" Simple Row:")
+
+        row = layout.row()
+        row.prop(scene, "frame_start")
+        row.prop(scene, "frame_end")
+
+        # Create an row where the buttons are aligned to each other.
+        layout.label(text=" Aligned Row:")
+
+        row = layout.row(align=True)
+        row.prop(scene, "frame_start")
+        row.prop(scene, "frame_end")
+
+        # Create two columns, by using a split layout.
+        split = layout.split()
+
+        # First column
+        col = split.column()
+        col.label(text="Column One:")
+        col.prop(scene, "frame_end")
+        col.prop(scene, "frame_start")
+
+        # Second column, aligned
+        col = split.column(align=True)
+        col.label(text="Column Two:")
+        col.prop(scene, "frame_start")
+        col.prop(scene, "frame_end")
+
+        # Big render button
+        layout.label(text="Big Button:")
+        row = layout.row()
+        row.scale_y = 3.0
+        row.operator("render.render")
+
+        # Different sizes in a row
+        layout.label(text="Different button sizes:")
+        row = layout.row(align=True)
+        row.operator("wm.test_gamepad")
+
+class WM_gamepad(bpy.types.Operator):
+    bl_idname = "wm.test_gamepad"
+    bl_label = "Test Gamepad"
+    bl_description = "Test function click me"
+
+    def execute(self, context: bpy.types.Context):
+
+        print("Finding gamepads...")
+        for device in devices:
+            print("Gamepads found", device)
+
+        return {"FINISHED"}
+
+def register():
+    bpy.utils.register_class(GamepadInputPanel)
+    bpy.utils.register_class(WM_gamepad)
+
+
+def unregister():
+    bpy.utils.unregister_class(GamepadInputPanel)
+    bpy.utils.unregister_class(WM_gamepad)
+
+
+if __name__ == "__main__":
+    register()
